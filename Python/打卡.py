0b17241db19acd35b5c99c17572dfbef5b330c0e
fetter1991 <610481663@qq.com>
2024-07-25 11:12:20 +0800

    打卡

--- Python/打卡.py
+++ Python/打卡.py
@@ -1,54 +1,82 @@
 import pyautogui
 import time
+from datetime import datetime, timedelta
 from playwright.sync_api import sync_playwright
 
+
+# playwright打开网页打卡
+# def checkOutByPlaywright(check_out_seconds):
+#     p = sync_playwright().start()
+#
+#     # 启动浏览器，返回 Browser 类型对象
+#     browser = p.chromium.launch(headless=False)
+#
+#     # 创建新页面，返回 Page 类型对象
+#     page = browser.new_page()
+#     page.goto("https://om.tencent.com/attendances/check_out/22700602")
+#     # 手动进行登录操作,check_out_seconds为多少秒后打卡
+#     page.wait_for_timeout(check_out_seconds)
+#
+#     # 点击签出按钮
+#     page.locator('#checkout_btn').click()  # 点击查询
+#
+#     page.wait_for_timeout(4000)
+#     page.locator('.btn-primary').click()  # 点击查询
+#
+#     # 关闭浏览器
+#     input('按下按钮Enter后才会执行，相当于一个断点')
+#     browser.close()
+#
+#     # 关闭 playwright driver 进程
+#     input('关闭 playwright driver 进程')
+#     p.stop()
+
+
+# 鼠标点击页面
 def checkOutByMouse():
-    print("程序开始执行")
+    # 1秒内将鼠标移动到(1470, 945)
+    # pyautogui.moveTo(1470, 945, duration=1)
+
+    print("程序开始执行打卡操作")
     # 改为自己需要的时间
-    time.sleep(15)
     pyautogui.click(x=1000, y=470)
     time.sleep(1)
     pyautogui.click(x=370, y=470)
     time.sleep(1)
-    pyautogui.click(x=1470, y=945)
+    # pyautogui.click(x=1470, y=945)
 
-    # # 1秒内将鼠标移动到(100, 100)
-    # pyautogui.moveTo(1000, 470, duration=1)
-    # # 在当前鼠标位置点击
-    # pyautogui.click()
-    #
-    # pyautogui.moveTo(370, 470, duration=1)
-    # pyautogui.click()
-    #
-    # pyautogui.moveTo(1470, 945, duration=1)
-    # pyautogui.click()
-    print("打开完成")
+    print("打卡完成")
 
 
-def checkOutByPlaywright():
-    p = sync_playwright().start()
+# 定时任务
+def countdown(check_out_seconds):
+    while check_out_seconds:
+        # 将时间差转换为小时、分钟和秒
+        hours, remainder = divmod(check_out_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
 
-    # 启动浏览器，返回 Browser 类型对象
-    browser = p.chromium.launch(headless=False)
+        timer = '{:02d}:{:02d}:{:02d}'.format(hours, minutes, seconds)
+        # print(timer, end="\r")
+        print(f"程序将在: {timer}后进行打卡操作", end="\r")
+        time.sleep(1)
+        check_out_seconds -= 1
+    print("打卡时间到！")
+    checkOutByMouse()
 
-    # 创建新页面，返回 Page 类型对象
-    page = browser.new_page()
-    page.goto("https://om.tencent.com/attendances/check_out/22700602")
-    # 打印网页标题栏
 
-    page.wait_for_timeout(4000)
+# 签入时间
+checkInStr = "2024-07-25 09:28"
+# 解析时间字符串A到datetime对象
+checkInTime = datetime.strptime(checkInStr, '%Y-%m-%d %H:%M')
 
-    # 输入通讯，点击查询。这是定位与操作，是自动化重点，后文详细讲解
-    page.locator('#checkout_btn').click()  # 点击查询
+# 在签入时间的基础上加上9个小时
+checkOutTime = checkInTime + timedelta(hours=1.7)
+print(f"签出时间: {checkOutTime.strftime('%Y-%m-%d %H:%M:%S')}")
 
-    # TODO 获取打卡时间，计算后再等待
-    page.wait_for_timeout(4000)
-    page.locator('.btn-primary').click()  # 点击查询
+# 获取当前时间
+current_time = datetime.now()
+# 计算签出时间与当前时间的时间差（以秒为单位）
+time_difference = int((checkOutTime - current_time).total_seconds())
 
-    input('调用browser.close()')
-    # 关闭浏览器
-    browser.close()
-
-    input('关闭 playwright driver 进程')
-    # 关闭 playwright driver 进程
-    p.stop()
+# 调用打卡主体程序
+countdown(time_difference)
