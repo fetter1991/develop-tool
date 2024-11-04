901b62f4b0e1ecf4ff09e39980c11a67516e693f
fetter1991 <610481663@qq.com>
2024-11-04 11:08:07 +0800

    初始化一个Java项目

---
+++ Java/eternum-world/source/service/eternum/src/test/java/com/eternum/acg/EternumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eternum.acg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EternumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
